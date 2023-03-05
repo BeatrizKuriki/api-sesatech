b2f60469419dd0fff641823269566a7951c0af7b
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2023-03-05 09:40:47 -0300

    feat: created project

---
+++ apirickymorty/src/test/java/com/sesatech/apirickymorty/ApirickymortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesatech.apirickymorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirickymortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
